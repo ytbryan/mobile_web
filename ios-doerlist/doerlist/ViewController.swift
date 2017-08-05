1fb4dd8decaa333a843ef4ea531664fc13e5f0ef
ytbryan <>
2017-08-05 14:04:44 +0800

    added ios directory back

---
+++ ios-doerlist/doerlist/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  doerlist
+//
+//  Created by ytbryan on 2/8/17.
+//  Copyright © 2017 tada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://app.doerlist.com");
+        let requestObj = NSURLRequest(url: url! as URL);
+        webView.loadRequest(requestObj as URLRequest);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
